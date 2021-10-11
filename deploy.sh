2b8994e3eabee1466ab314f760be0d0c0341c9a9
Sabso <t.bassobondini@gmail.com>
2021-10-11 11:32:23 +0400

    fix vueconfig

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:sabso7/theobassobondini.git master:gh-pages
+git push -f git@github.com:sabso7/casapizza.git master:gh-page
 
 cd -
